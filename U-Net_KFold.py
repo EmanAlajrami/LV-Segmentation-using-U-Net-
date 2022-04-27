ca6b778984ad15a7dec050ff7933f25005352c82
Eman Alajrami <52444021+EmanAlajrami@users.noreply.github.com>
2022-04-27 12:31:31 +0100

    Update U-Net_KFold.py

--- U-Net_KFold.py
+++ U-Net_KFold.py
@@ -1,1 +1,9 @@
 
+# ***********/Trying to use K_Fold cross validation /*******
+
+EPOCHS = 15
+OUTPUT_CHANNELS = 1 # no of classes
+input_shape_image = [512,512,1] #[1024, 1024, 1]
+BATCH_SIZE = 2
+BUFFER_SIZE = 1000
+do_training = True
